341c58c8d41b7296216a22a4f426608f4b14c013
Ronald Ligteringen <r.ligteringen@gmail.com>
2019-11-06 15:13:18 +0100

    added hello world

--- workshop/workshop.py
+++ workshop/workshop.py
@@ -8,3 +8,6 @@ logger = logging.getLogger(__name__)
 def example():
     logger.info('Providing information about the execution of the function.')
     print('Hello workshop!')
+
+if __name__ == '__main__':
+    example()
