c85e8370cbbb5b5924a3ace99ee22dd1963bd5bb
Ronald Ligteringen <r.ligteringen@gmail.com>
2019-11-07 11:45:58 +0100

    test for travis

--- workshop/workshop.py
+++ workshop/workshop.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 # FIXME: put actual code here
 def example():
     logger.info('Providing information about the execution of the function.')
-    print('Hello workshop!')
+    print('Hello workshop! How are you doing?')
 
 if __name__ == '__main__':
     example()
